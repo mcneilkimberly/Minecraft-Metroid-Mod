3aed10a8797aca92732b48fe7cba842cbb4b4856
Pikalyx <78184925+Pikalyx@users.noreply.github.com>
2024-12-14 21:45:23 -0500

    Base mod using the template mod
    Adds the starter files used for the mod

---
+++ src/client/java/io/github/pikalyx/cubetroidmod/CubetroidModDataGenerator.java
@@ -0,0 +1,11 @@
+package io.github.pikalyx.cubetroidmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CubetroidModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
