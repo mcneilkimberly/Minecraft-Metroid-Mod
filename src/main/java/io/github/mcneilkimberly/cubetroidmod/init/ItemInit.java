d277fb631e596c64626f4af692796b3b6535f54d
Pikalyx <78184925+Pikalyx@users.noreply.github.com>
2024-12-16 16:37:01 -0500

    Changes to item
    Changed it to a chozite bar

--- src/main/java/io/github/mcneilkimberly/cubetroidmod/init/ItemInit.java
+++ src/main/java/io/github/mcneilkimberly/cubetroidmod/init/ItemInit.java
@@ -6,7 +6,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 
 public class ItemInit {
-    public  static final Item EXAMPLE_ITEM = register("example_item", new Item(new Item.Settings()));
+    public  static final Item CHOZITE_BAR = register("chozite_bar", new Item(new Item.Settings()));
 
     public static <T extends Item> T register(String name, T item){
         return Registry.register(Registries.ITEM, Cubetroid.id(name), item);
